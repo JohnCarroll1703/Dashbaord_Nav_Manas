d9af77326f4049c612bc7b0fa50dd9627cc1b198
aljan2000 <aljan2000@mail.ru>
2024-02-25 13:10:51 +0600

    Project

---
+++ app/src/main/java/com/example/dashbaord_nav_manas/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.dashbaord_nav_manas;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout= findViewById(R.id.drawerlayout);
+        navigationView = findViewById(R.id.navigationview);
+        toolbar = findViewById(R.id.toolbar);
+
+
+    }
+}
